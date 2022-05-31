7ea72c53f20e742b097b5e145018b69f06a32ca3
Jarren San Jose <sanjose.jarren@gmail.com>
2022-05-31 14:20:51 -0700

    add placeholder text

--- pages/index.tsx
+++ pages/index.tsx
@@ -58,6 +58,7 @@ const Home: NextPage = () => {
           }}
           name=""
           id=""
+          placeholder={normalTextToGoblinText("Enter text here")}
           cols={30}
           rows={5}
           onChange={(e) => {
